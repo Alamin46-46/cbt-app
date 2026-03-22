0b6f99b75da7b8c2b29e1b71b94a80c888038ba5
Alamin46-46 <AlaminBashir814@gmail.com>
2026-03-22 17:57:02 +0100

    questions added

--- app.py
+++ app.py
@@ -7,7 +7,8 @@ quiz = Quiz()
 
 # Sample questions
 quiz.add_question(Question("What is 2 + 2?", ["2", "3", "4", "5"], "4"))
-quiz.add_question(Question("Capital of Nigeria?", ["Lagos", "Abuja", "Kano", "Ibadan"], "Abuja"))
+quiz.add_question(Question("what state is the Capital of Nigeria?", ["Lagos", "Abuja", "Kano", "Ibadan"], "Abuja"))
+quiz.add_question(Question("who is the current governor of Kano?", ["Kwankwaso", "Ganduje", "Abba K Yusuf", "Gawuna"], "Abba K Yusuf"))
 
 @app.route('/')
 def home():
